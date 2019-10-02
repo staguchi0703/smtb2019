ae2e2847de852cae1c8adbac798ce073fbdd11c5
sho Taguchi <s.taguchi0703@gmail.com>
2019-10-02 22:36:31 +0900

    A~Fのテンプレートを作成

---
+++ E/E.py
@@ -0,0 +1,21 @@
+# %%
+# VScodeで入力をテキストから読み込んで標準入力に渡す
+import sys
+import os
+f=open(r'.\E\E_input.txt', 'r', encoding="utf-8")
+# inputをフルパスで指定
+# win10でファイルを作るとs-jisで保存されるため、読み込みをutf-8へエンコードする必要あり
+# VScodeでinput file開くとutf8になってるんだけど中身は結局s-jisになっているらしい
+sys.stdin=f
+
+#
+# 入力スニペット
+# num = int(input())
+# num_list = [int(item) for item in input().split()]
+# num_list = [input() for _ in range(3)]
+##################################
+# %%
+# 以下ペースト可
+num_list = [int(item) for item in input().split()]
+print('E', num_list)
+
